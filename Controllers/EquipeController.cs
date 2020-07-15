475a7b45e3fca64eecf06e6cae6be2ff29f2eda7
DharoOficial <erickbandeiradharo@gmail.com>
2020-07-15 11:31:37 -0300

    projeto em andamento : upload e Delete de imagem feito

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -24,11 +25,41 @@ namespace E_Players_Asp_Net_Core.Controllers
             Equipe novaEquipe = new Equipe();
             novaEquipe.IdEquipe = Int32.Parse( form ["IdEquipe"] );
             novaEquipe.Nome = form["Nome"];
-            novaEquipe.Imagem = form["Imagem"];
+            
+
+             // Upload Início
+            var file    = form.Files[0];
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                novaEquipe.Imagem   = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem   = "padrao.png";
+            }
+            // Upload Final
 
             equipeModel.Create(novaEquipe);
             ViewBag.Equipes = equipeModel.ReadAll();
             return LocalRedirect("~/Equipe");
+        }
+        [Route("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            equipeModel.Delete(id);
+            return LocalRedirect("~/Equipe");
+
         }
 
     }
